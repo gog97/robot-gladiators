63797d57fb37485e3395b39a4308f37b18a74897
gzflykid <ozgarcia97@gmail.com>
2021-08-03 16:07:25 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -88,7 +88,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
   }
-  // if player isn't alive, stop the game
+  // if player isn't alive, stop the game.
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
